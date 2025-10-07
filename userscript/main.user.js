147377023ead61b435e9d145cb11c1554a65d987
Nicholas Doherty <nicholasdoherty95@gmail.com>
2025-10-06 22:17:22 -0400

    Add scaffolding

---
+++ userscript/main.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         WPD Blackjack Automation
+// @namespace    https://github.com/adastra1826
+// @version      1.0
+// @description  Automated blackjack playing with Flask backend
+// @author       Nicholas Doherty
+// @match        https://watchpeopledie.tv/casino/blackjack*
+// @grant        GM_xmlhttpRequest
+// @grant        unsafeWindow
+// @connect      localhost
+// @run-at       document-start
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    console.log('WPD Blackjack Automation script beginning loading');
+})();
